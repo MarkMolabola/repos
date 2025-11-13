bc9352d8a2abb89a7f99115c2e8112c2f72864b0
MarkMolabola <mark.molabola@outlook.com>
2025-11-12 16:11:58 -0800

    uipdate

---
+++ Longest Common Prefix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Longest_Common_Prefix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "flower", "flow", "flight" };
+            string result = LongestCommonPrefix(strings);
+        }
+
+        
+
+            static string LongestCommonPrefix(string[] strs)
+        {
+
+
+            if (strs == null || strs.Length == 0) return "";
+
+            int i = 0;
+            while (true)
+            {
+                if (i >= strs[0].Length) break;
+                char currentChar = strs[0][i];
+
+                foreach (string word in strs)
+                {
+                    if (i >= word.Length || word[i] != currentChar)
+                    {
+                        return strs[0].Substring(0, i);
+                    }
+                }
+
+                i++;
+            }
+
+            return strs[0].Substring(0, i);
+
+        }
+    }
+}
--- Single Number/Program.cs
+++ Single Number/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] ints = new int[] { 4, 1, 2, 1, 2 };
+            SingleNumber(ints);
+        }
+        static int SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (int num in nums)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict.Remove(num);
+                }
+                else
+                {
+                    dict[num] = 1;
+                }
+            }
+            return dict.Keys.First();
         }
     }
 }
